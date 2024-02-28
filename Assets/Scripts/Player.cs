6c3549fd5a440f0115fcfe02cf23762c3c010bce
JohanLeoBenZam <yojan.benzam@gmail.com>
2024-02-28 09:49:49 +0100

    tengo sueño
    xd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Player : MonoBehaviour
 {
@@ -10,17 +15,16 @@ public class Player : MonoBehaviour
     SpriteRenderer sprite;
 
     float h, v;
-    
+
     //propiedades
     public float fuerzaMov;
     public float fuerzaSalto;
     public int saltosMax;
-   
-        
+
     public LayerMask queEsSuelo;
 
-    private enum MovementState {idle, running, jumping, falling }
-    
+    private enum MovementState { idle, running, jumping, falling }
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -29,51 +33,83 @@ public class Player : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
     }
 
+
+    void Update()
+{
+
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        h = Input.GetAxisRaw("Horizontal");
+    v = Input.GetAxisRaw("Vertical");
+
+    rb.velocity = new Vector2(h * fuerzaMov, rb.velocity.y);
+
+    if (Input.GetButtonDown("Vertical") && IsGrounded())
+    {
+        rb.velocity = new Vector2(rb.velocity.x, fuerzaSalto);
+    }
+
+    UpdateAnimation();
+}
+
+#elif UNITY_ANDROID || UNITY_IOS
     void Update()
     {
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
-
-        rb.velocity = new Vector2(h * fuerzaMov, rb.velocity.y);
-        
-
-        if (Input.GetButtonDown("Vertical") && IsGrounded())
+        // Verificar entrada táctil
+        if (Input.touchCount > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x,fuerzaSalto);
+            foreach (Touch touch in Input.touches)
+            {
+                // Dividir la pantalla en dos zonas (izquierda y derecha)
+                if (touch.position.x < Screen.width / 2)
+                {
+                    // Mover al personaje de izquierda a derecha
+                    float moveDelta = touch.deltaPosition.x * Time.deltaTime;
+                    transform.Translate(new Vector3(moveDelta, 0, 0));
+                }
+                else
+                {
+                    // Saltar al tocar la zona derecha de la pantalla
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        Jump();
+                    }
+                }
+            }
         }
-
-        UpdateAnimation();
     }
+#endif
 
-    private bool IsGrounded()
+private bool IsGrounded()
+{
+    return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, queEsSuelo);
+}
+
+private void UpdateAnimation()
+{
+    MovementState state;
+    if (h > 0f)
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, queEsSuelo);
+        state = MovementState.running;
+        sprite.flipX = false;
     }
-    private void UpdateAnimation()
+    else if (h < 0f)
     {
-        MovementState state;
-        if (h > 0f)
-        {
-            state = MovementState.running;
-            sprite.flipX = false;
-        }
-        else if (h < 0f)
-        {
-            state = MovementState.running;
-            sprite.flipX = true;
-        }
-        else
-        {
-            state = MovementState.idle;
-        }
-        if (rb.velocity.y > .1f)
-        {
-            state = MovementState.jumping;
-        }
-        else if (rb.velocity.y < -0.1f)
-        {
-            state = MovementState.falling;
-        }
-        anim.SetInteger("state", (int)state);
+        state = MovementState.running;
+        sprite.flipX = true;
     }
+    else
+    {
+        state = MovementState.idle;
+    }
+    if (rb.velocity.y > .1f)
+    {
+        state = MovementState.jumping;
+    }
+    else if (rb.velocity.y < -0.1f)
+    {
+        state = MovementState.falling;
+    }
+    anim.SetInteger("state", (int)state);
+}
 }
