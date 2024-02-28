9b6d269d68a5ebedd2cd99bd82444185254cf3f6
JohanLeoBenZam <yojan.benzam@gmail.com>
2024-02-28 01:30:43 +0100

    mi pana en el baño

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
     private Animator anim;
+    private Rigidbody2D rb;
     private void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,6 +23,13 @@ public class PlayerLife : MonoBehaviour
 
     private void Die()
     {
+
+        rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("death");
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
